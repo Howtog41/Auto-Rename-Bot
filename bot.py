7d80cd7977e372425fc59b6c7d56d40a5ad76ec3
Howtog41 <howtog41@gmail.com>
2024-09-30 16:50:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 import logging
-
 import warnings
 from pyrogram import Client, idle
-from pyrogram import Client, __version__
+from pyrogram import __version__
 from pyrogram.raw.all import layer
 from config import Config
 from aiohttp import web
@@ -12,10 +11,8 @@ import asyncio
 from plugins.web_support import web_server
 import pyromod
 
-
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
-
 
 class Bot(Client):
     def __init__(self):
@@ -28,16 +25,20 @@ class Bot(Client):
             plugins={"root": "plugins"},
             sleep_threshold=15,
         )
+        self.app = None  # Initialize app as an instance variable
 
     async def start(self):
         await super().start()
         me = await self.get_me()
         self.mention = me.mention
         self.username = me.username
-        app = web.AppRunner(await web_server())
-        await app.setup()
+
+        # Define app here
+        self.app = web.AppRunner(await web_server())
+        await self.app.setup()
         bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, Config.PORT).start()
+        await web.TCPSite(self.app, bind_address, Config.PORT).start()
+
         logging.info(f"{me.first_name} ✅✅ BOT started successfully ✅✅")
 
         for id in Config.ADMIN:
@@ -61,18 +62,17 @@ class Bot(Client):
                 print("Pʟᴇᴀꜱᴇ Mᴀᴋᴇ Tʜɪꜱ Iꜱ Aᴅᴍɪɴ Iɴ Yᴏᴜʀ Lᴏɢ Cʜᴀɴɴᴇʟ")
 
     async def stop(self, *args):
+        if self.app:
+            await self.app.cleanup()  # Clean up the app when stopping
         await super().stop()
         logging.info("Bot Stopped 🙄")
 
-
 bot_instance = Bot()
-
 
 def main():
     async def start_services():
         if Config.STRING_SESSION:
             await asyncio.gather(
-                app.start(),  # Start the Pyrogram Client
                 bot_instance.start(),  # Start the bot instance
             )
         else:
@@ -81,7 +81,6 @@ def main():
     loop = asyncio.get_event_loop()
     loop.run_until_complete(start_services())
     loop.run_forever()
-
 
 if __name__ == "__main__":
     warnings.filterwarnings("ignore", message="There is no current event loop")
